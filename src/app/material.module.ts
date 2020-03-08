7f36447ad10f89896870044962244c2f5e303caa
demaneno <61903384+demaneno@users.noreply.github.com>
2020-03-08 22:13:41 +0100

    removed unused modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,19 +6,19 @@ import { LayoutModule } from '@angular/cdk/layout';
 //
 
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatSliderModule } from '@angular/material/slider';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatDividerModule } from '@angular/material/divider';
 import { MatBadgeModule } from '@angular/material/badge';
 
 
@@ -31,14 +31,13 @@ import { MatBadgeModule } from '@angular/material/badge';
 const modules: any[] = [
 
   LayoutModule,  MatAutocompleteModule,
-  MatCheckboxModule,  MatDatepickerModule,
+  // MatCheckboxModule,  MatDatepickerModule,
   MatFormFieldModule,  MatInputModule,
-  MatRadioModule,  MatSelectModule,
-  MatSliderModule,  MatSlideToggleModule,
+  // MatRadioModule,  MatSelectModule,
+  // MatSliderModule,  MatSlideToggleModule,
   MatButtonModule,  MatToolbarModule,
-  MatIconModule,  MatCardModule, 
-  MatBadgeModule, MatDividerModule,
-  MatInputModule
+  MatIconModule,  MatBadgeModule,
+  // MatCardModule, MatDividerModule,
 //   MatNativeDateModule,
   // MatMomentDateModule,
 
